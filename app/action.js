24af7a8e1543618afe41a68540abc3f0bf964434
Chowdhurian <manil.chowdhury@gmail.com>
2016-05-19 20:32:40 -0700

    twitter stream outputting to console

--- app/action.js
+++ app/action.js
@@ -21,38 +21,34 @@ router.get('/', function(req, res, next) {
 router.get('/results/:search_term?', function(req, res, next) {
 	var searchTerm = req.params.search_term || 'cute wombats'
 
-	// 	twit.stream('statuses/filter',{track: 'nodejs'},function(stream){
-	// 	stream.on('data',function(data){
-	// 		// console.log(data);
-	// 		var tweet = {
-	// 	 		twid: data["id_str"],
-	// 	 		author: data["user"]["name"],
-	// 	 		avatar: data["user"]["profile_image_url"],
-	// 	 		text: data["text"],
-	// 	 		date: data['created_at'],
-	//         	screenname: data['user']['screen_name']
-	// 	 	};
-	// 		 res.render('twitter',{tweets: tweet});
-	// 	 });
-	// });
+	twitter.stream('statuses/filter', {track: 'donald trump'}, function(stream) {
+		stream.on('data',function(data){
+			console.log(data);
 
-	intlpedia.search(searchTerm)
-	  .then(page => console.log(page))
-	  .catch(err => console.error(err));
+			// var tweet = {
+			// 		twid: data["id_str"],
+			// 		author: data["user"]["name"],
+			// 		avatar: data["user"]["profile_image_url"],
+			// 		text: data["text"],
+			// 		date: data['created_at'],
+	    //   screenname: data['user']['screen_name']
+			// 	};
+			//
+			// res.render('index', {tweets: tweet});
+		 });
+	});
+
+	// intlpedia.search(searchTerm)
+	//   .then(page => console.log(page))
+	//   .catch(err => console.error(err));
 
 	youTube.setKey(config.youtube.consumer_key);
 	youTube.addParam('relevanceLanguage', 'en');
 
-	var youTubePromise = new Promise(function(resolve, reject) {
-
-	});
-
 	youTube.search(searchTerm, 2, function(error, result) {
-		if (error) {
-			console.log(error);
-		} else {
-			var videos = [];
+		var videos = [];
 
+		function parseResult(result) {
 			for (var i = 0; i < result["items"].length; i++) {
 				videos.push({
 					video_id: result["items"][i].id.videoId,
@@ -61,7 +57,12 @@ router.get('/results/:search_term?', function(req, res, next) {
 					description: result["items"][i].snippet.description
 				});
 			}
+		}
 
+		if (error) {
+			console.log(error);
+		} else {
+			parseResult(result)
 			res.render('index', {videos: videos});
 		}
 	});
