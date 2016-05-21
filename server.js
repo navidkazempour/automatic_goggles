206416725997866c4c6d58abcc9ef5161b68b0b9
am52saif <aliasgar.s.merchant@gmail.com>
2016-05-21 18:07:29 +0000

    Added Favicon

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var path = require('path');
 var parser = require('body-parser');
+var favicon = require('serve-favicon');
 
 var app = express();
 var router = require('./app/action');
 var port = process.env.port || 3000;
 
 require('./src/database');
-console.log(path.join(__dirname,'views'));
 app.set('views', path.join(__dirname,'/app/views'));
 app.set('view engine','jade');
 app.use(express.static(path.join(__dirname,'public')));
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(parser.json());
 app.use('/',router);
 app.listen(port,function(){
