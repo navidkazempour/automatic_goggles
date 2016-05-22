6a0938fad43e3f36ba84e39c74f54c3cfef78700
Chowdhurian <manil.chowdhury@gmail.com>
2016-05-21 21:03:15 -0700

    alchemy code working, receives json back from api

--- app/action.js
+++ app/action.js
@@ -138,4 +138,32 @@ router.post('/twitter/:search_term?', function(req, res) {
   });
 });
 
+//Create the AlchemyAPI object
+var AlchemyAPI = require('../alchemyapi');
+var alchemyapi = new AlchemyAPI();
+
+router.get('/alchemy/what', doAlchemy);
+var what_url = 'http://www.npr.org/2013/11/26/247336038/dont-stuff-the-turkey-and-other-tips-from-americas-test-kitchen';
+var demo_text = 'Yesterday dumb Bob destroyed my fancy iPhone in beautiful Denver, Colorado. I guess I will have to head over to the Apple Store and buy a new one.';
+
+function doAlchemy(req, res) {
+	var output = {};
+
+  function entities(req, res, output) {
+  	// alchemyapi.entities('url', what_url,{ 'sentiment':1 }, function(response) {
+  	// 	output['entities'] = { url:what_url, response:JSON.stringify(response,null,4), results:response['entities'] };
+    alchemyapi.entities('text', demo_text,{ 'sentiment':1 }, function(response) {
+  		output['entities'] = { text:demo_text, response:JSON.stringify(response,null,4), results:response['entities'] };
+      console.log('Alchemy is real');
+      console.log(output);
+  	});
+  }
+
+  //Start the analysis chain
+	entities(req, res, output);
+
+  // res.render('index',output);
+  res.render('index');
+};
+
 module.exports = router;
