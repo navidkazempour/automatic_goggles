8ea53353d257a1fda099675ff57a3528e34f07a5
am52saif <aliasgar.s.merchant@gmail.com>
2016-05-24 06:49:21 +0000

    Arranging Codes - Neat Routes

--- API/youtube.js
+++ API/youtube.js
@@ -1,16 +1,28 @@
+var config = require('../config');
 var Youtube = require('youtube-node');
-
 var youTube = new Youtube();
-
-youTube.setKey('AIzaSyBWDQ2uae9ojRM-lOHaL1qqyFJCF3B_P7A');
-
+youTube.setKey(config.youtube);
 youTube.addParam('relevanceLanguage', 'en');
 
-youTube.search('World War z Trailer',10,function(error, result) {
-  if (error) {
-    console.log(error);
-  }
-  else {
-    console.log(JSON.stringify(result,null,2));
-  }
-});
+var vids = [];
+
+var videos = function(searchTerm,callback){
+  youTube.search(searchTerm,5,function(error, result) {
+    if (error) {
+      console.log(error);
+      }
+      else {
+        for(var i=0;i<result["items"].length;i++){
+          vids.push({
+            video_id: result["items"][i].id.videoId,
+            date: result["items"][i].snippet.publishedAt,
+            title: result["items"][i].snippet.title,
+            description: result["items"][i].snippet.description
+          });
+        }
+        callback(vids);
+      }
+  });
+};
+
+module.exports = videos;
